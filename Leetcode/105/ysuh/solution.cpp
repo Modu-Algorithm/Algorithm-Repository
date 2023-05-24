acbf7c1d8291f82af6f5346a4d3f3f4aea2dcc52
Youngsang Suh <60172447+youngsangsuh@users.noreply.github.com>
2023-05-24 15:54:49 -0700

    Update solution.cpp

--- Leetcode/105/ysuh/solution.cpp
+++ Leetcode/105/ysuh/solution.cpp
@@ -11,68 +11,25 @@
  */
 class Solution {
 public:
-    TreeNode root;
-    TreeNode* root_pointer = &root;
-    
-    int findRight(int pivot, vector<int>& preorder, vector<int>& inorder) {
-        int start_idx = 2;
-        while (true) {
-            if (start_idx == preorder.size()) return -1;
-            for (int idx=0; idx<inorder.size(); idx++) {
-                if (pivot == inorder[idx]) {
-                    start_idx++;
-                    continue;
-                }
-                if (preorder[start_idx] == inorder[idx]) {
-                    return start_idx;
-                }
-            }
+    map<int, int> val_idx_map;
+    int idx = 0;
+    TreeNode* find(vector<int>& preorder, vector<int>& inorder, int left, int right) {
+        if (left > right) {
+            return nullptr;
         }
-    }
-    TreeNode* traverseTree(TreeNode* parent, vector<int>& preorder, vector<int>& inorder) {
-        
-        // parent->val = preorder[0];
-        // parent->val = 0;
-        // if (preorder.size() > 1) {
-        //     for (int i=0; i<inorder.size(); i++) {
-        //         if (preorder[1] == inorder[i]) {
-        //             TreeNode left(preorder[1]);
-        //             parent->left = &left;
-        //             if (preorder.size() > 2) {
-        //                 int right_idx = findRight(preorder[0], preorder, inorder);
-        //                 if (right_idx == -1) {
-        //                     parent->right = NULL;
-        //                 } else {
-        //                     TreeNode right(preorder[right_idx]);
-        //                     parent->right = &right;
-        //                 }
-        //             } else {
-        //                 parent->right = NULL;
-        //             }
-        //             preorder.erase(preorder.begin());
-        //             inorder.erase(inorder.begin()+i);
-        //         }
-        //         if (preorder[0] == inorder[i]) {
-        //             parent->left = NULL;
-        //             TreeNode right(preorder[1]);
-        //             parent->right = &right;
-        //             preorder.erase(preorder.begin());
-        //             inorder.erase(inorder.begin()+i);
-        //             break;
-        //         }
-        //     }
-        // }
-        // if (parent->left != NULL) {
-        //     traverseTree(parent->left, preorder, inorder);
-        // }
-        // if (parent->right != NULL) {
-        //     traverseTree(parent->right, preorder, inorder);
-        // }
+        int i_idx = val_idx_map[preorder[idx]];
+        TreeNode* parent = new TreeNode(preorder[idx]);
+        idx++;
+        parent->left = find(preorder, inorder, left, i_idx-1);
+        parent->right = find(preorder, inorder, i_idx+1, right);
         return parent;
     }
-
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        traverseTree(root_pointer, preorder, inorder);
-        return root_pointer;
+        int idx = 0;
+        for (auto ele : inorder) {
+            val_idx_map[ele] = idx;
+            idx++;
+        }
+        return find(preorder, inorder, 0, inorder.size()-1);
     }
 };
